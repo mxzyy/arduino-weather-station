08cc01b9e9c507183ab571339fa44e12015d27aa
mxzyy <94878524+mxzyy@users.noreply.github.com>
2022-12-04 16:24:40 +0700

    Update sketch_nov27a.ino

--- sketch_nov27a.ino
+++ sketch_nov27a.ino
@@ -6,61 +6,56 @@
 //  \  /\  /  __/ (_| | |_| | | |  __/ |    _\ \ |_| \__ \ ||  __/ | | | | \__ \
 //   \/  \/ \___|\__,_|\__|_| |_|\___|_|    \__/\__, |___/\__\___|_| |_| |_|___/
 //                                              |___/                           
+ 
 // ----
 // WEATHER STATION 
 // Created by @mxzyy
 // ----
+
+
 #define BLYNK_PRINT Serial
 #define BLYNK_TEMPLATE_ID "TMPLmaHjKFz8"
 #define BLYNK_DEVICE_NAME "Weather System"
 #define BLYNK_AUTH_TOKEN "qf8uk1uT6tmvodHSD5H_fh6GNPOdf7fH"
+#define DHTPIN 12          
+#define DHTTYPE DHT22 
  
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 #include <DHT.h>
- 
-// init 
-char auth[] = "qf8uk1uT6tmvodHSD5H_fh6GNPOdf7fH"; 
-char ssid[] = "andaikan";
-char pass[] = "12345678";
- 
-#define DHTPIN 12          // D3
- 
-// Uncomment whatever type you're using!
-//#define DHTTYPE DHT11     // DHT 11
-#define DHTTYPE DHT22   // DHT 22, AM2302, AM2321
-//#define DHTTYPE DHT21   // DHT 21, AM2301
- 
-DHT dht(DHTPIN, DHTTYPE);
-BlynkTimer timer;
 #include <LiquidCrystal_I2C.h>
 
-// set the LCD number of columns and rows
+DHT dht(DHTPIN, DHTTYPE);
+BlynkTimer timer;
 int lcdColumns = 16;
 int lcdRows = 2; 
 LiquidCrystal_I2C lcd(0x3F, lcdColumns, lcdRows);  
+
+// Wi - Fi Configuration
+char auth[] = "qf8uk1uT6tmvodHSD5H_fh6GNPOdf7fH"; 
+char ssid[] = "4RBIK TEMULAWAK";
+char pass[] = "H3rb4l04";
+
+
 void sendSensor()
 {
   float h = dht.readHumidity();
-  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+  float t = dht.readTemperature(); 
  
   if (isnan(h) || isnan(t)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
-  // You can send any value at any time.
-  // Please don't send more that 10 values per second.
+ 
   Blynk.virtualWrite(V0, t);
   Blynk.virtualWrite(V1, h);
 }
  
 void setup()
 {
-  // Debug console
+
   Serial.begin(9600);
- 
   Blynk.begin(auth, ssid, pass);
-  
  
   dht.begin();
   lcd.init();
@@ -71,13 +66,11 @@ void setup()
  
 void loop()
 {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); 
+  
   Blynk.run();
   timer.run();
-  float t = dht.readTemperature();
-  float h = dht.readHumidity();
-  Serial.print("Temp : ");
-  Serial.print(t);
-  Serial.println(" ");
   lcd.setCursor(0,0);
   lcd.print("SUHU : ");
   lcd.print(t);
